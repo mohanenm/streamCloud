4b22c497741bc21737bbd5e6491fc2386a9ec448
mohanenm <ethannmohan@gmail.com>
2018-09-07 14:47:44 -0500

    added K&R word count, added cmmts

--- split/read.c
+++ split/read.c
@@ -6,5 +6,28 @@
 #define IN 1 
 #define OUT 0
 
+main() {
 
+int c, n1, nw, nc, state; 
 
+/* set initial state to being outside a word */
+
+state = OUT;
+
+/* all variables set to 0 */ 
+ 
+n1 = nw = nc = 0;
+
+while((c = getchar()) != EOF) {
+	++nc; 
+	if(c == '\n')
+		++n1; 
+	if(c == ' ' || c == '\n' || c == '\t')
+		state = OUT; 
+	else if (state == OUT); 
+		state = IN;
+		++nw;
+		}
+	}
+	printf("%d %d %d\n", n1, nw, nc); 	
+}
