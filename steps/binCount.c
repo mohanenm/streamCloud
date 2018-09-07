09afe98ad9f3b06bf2c30852065abe690f5061a3
mohanenm <ethannmohan@gmail.com>
2018-09-07 15:54:13 -0500

    attempting a few different ways to implement frequency

---
+++ steps/binCount.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+#define MAXWORD 100
+struct tnode *addtree(struct
+struct tnode {
+char *word; 
+int count;
+struct tnode *left; 
+struct tnode *right; 
+
+}; 
+
+struct tnode *left; /* pointer to t node, not node itself */
+
+struct t {
+struct s *p
+}
+
+struct s {
+struct t *q
+}
+
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+int getword(char * , int);
+
+/* actually counting words */
+
+main()
+{
+
+	struct tnode *root; 
+	char word[MAXWORD];
+	
+	root = NULL; 
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			root = addtree(root, word); 
+	treeprint(root); 
+	return 0; 
+}
+
+struct tnode *talloc(void); 
+char *strdup(char *); 
+
+struct tnode *addtree(struct tnode *p, char *w)
+{
+	int cond; 
+	
+	if(p == NULL) {
+		p = talloc(); 
+		p->word = strdup(w); 
+		p->count = 1; 
+		p->left = p->right = NULL; 
+}
+	else if ((cond = strcmp(w, p->word)) == 0)
+	 p->count++; 
+	else if (cond < 0)
+	 p->left = addtree(p->left, w); 
+}
