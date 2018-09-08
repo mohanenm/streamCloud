304c13a6c461e76e66f4e322dbb2a3ecd578658b
mohanenm <ethannmohan@gmail.com>
2018-09-08 16:49:00 -0500

    tryng to solve all chapter 6 exercises

--- steps/binCount.c
+++ steps/binCount.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
-#include<ctype.h>
-#include<string.h>
-#include "tree.h"
+#include <ctype.h>
+#include <string.h>
+/* #include "tree.h" */
+#include <stdlib.h>
 
-main()
+#define MAXWORD 100
+
+struct tnode {
+ char *word; int count; struct *left; struct right; 
+}; 
+
+int wordCount = 0; 
+struct tnode **tword; 
+ 
+struct tnode *addtree(dtuct tnode *p, char *w); 
+void extract(struct tnode *p); 
+int freqcmp(const void *a, const void *b); 
+int getword(char *word, int lim); 
+
+int main()
 {
 
 	struct tnode *root; 
 	char word[MAXWORD];
-	
+	int i; 
+
 	root = NULL; 
 	while (getword(word, MAXWORD) != EOF)
 		if (isalpha(word[0]))
 			root = addtree(root, word); 
-	treeprint(root); 
+	
+
+	tword = (struct tnode**) malloc(sizeof(struct tnode) * wordCount); 
+
+	wordCount = 0; 
+	extract(root); 
+
+	qsort(tword, wordCount, sizeof tword[0], freqcmp); 
+	
+	/* actually print these after having extract all words into tword */
+
+	for(i = 0; i < wordCount ; i++)
+
+		printf("%4d %d\n", tword[i]->count, tword[i]->word); 
+
 	return 0; 
 }
 
 struct tnode *talloc(void); 
-/* char *strdup(char *); */
+char *strdup(char *s); 
 
-struct tnode *addtree(struct tnode *p, char *w)
+struct tnode *addtree (struct tnode *p, char *w)
+
+/*
+struct tnode *talloc(void); 
+ char *strdup(char *);
+*/
+
 {
 	int cond; 
 	
@@ -29,6 +65,9 @@ struct tnode *addtree(struct tnode *p, char *w)
 		p->word = strdup(w); 
 		p->count = 1; 
 		p->left = p->right = NULL; 
+
+		wordCount++; /*count distinct words, this will be integral to completing the assignment */
+
 }
 	else if ((cond = strcmp(w, p->word)) == 0)
 	 p->count++; 
@@ -38,13 +77,86 @@ struct tnode *addtree(struct tnode *p, char *w)
 	 p->right = addtree(p->right, w); 
 	return p;
 }
+struct tnode *talloc (void)
+{
+return (struct tnode *) malloc(sizeof(struct tnode)); 
+}
+char *strdup(char *s) 
+{
+	char *p; 
+	if((p=(char *) malloc(strlen(s) +1)) !=NULL)
+		strcpy(p,s);
+	return p; 
+}
 
-void treeprint(struct tnode *p)
+
+void extract (struct tnode *p)
 {
 	if (p != NULL) {
 	treeprint(p->left); 
-	printf("%4d %s\n", p->count, p->word); 
+	tword[wordCount++] = p;  
 	treeprint(p->right); 
 	}
 }
+int freqcmp(const void *a, const void *b)
+{
+	struct tnode **p, **q;
 
+	p = (struct tnode **) a; 
+	q = (struct tnode **) b;
+	
+	return (*q)->count - (*p) ->count;  
+}
+
+int getch(void); 
+void ungetch(int c); 
+
+
+int getword(char *word, int lim)
+{
+
+	int c; char *w = word; 
+
+	while(isspace(c = getch()))
+
+; 
+
+if ( c != EOF) *w++ = c; 
+
+if(!isalpha(c)) {
+*w = '\0';
+return c; 
+}
+
+for ( ; --lim > 1; w++)
+
+	if(isalnum(*w = getch())) {
+
+		ungetch(*w); 
+		break; 
+	}
+
+	*w = '\0';
+
+	return word[0];
+
+}
+
+
+/*
+define BUFSIZE 100
+int bufp = 0; 
+
+int getch (void)
+ {
+
+	return bufp > 0 ? buf[--bufp] : getchar(); 
+}
+
+void ungetch(int c) 
+
+{
+
+	if (bufp >= BUFSIZE)
+ 
+*/
