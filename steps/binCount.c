d819234010fe5b11cf499df0177a1b0337c1c000
mohanenm <ethannmohan@gmail.com>
2018-09-08 18:33:24 -0500

    ok, getting closer, no warnings or errors, but behaviour is not correct yet

--- steps/binCount.c
+++ steps/binCount.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <ctype.h>
-/* #include "tree.h" */
+/*#include "tree.h" */
 #include <stdlib.h>
+#include <string.h>
 
 
 #define MAXWORD 100
+/* char *strdup(char *s)
+{
+        char *p;
+        if((p=(char *) malloc(strlen(s) +1)) !=NULL)
+                strcpy(p,s);
+        return p;
+} */
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch (void)
+ {
+
+        return bufp > 0 ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+
+{
+
+        if (bufp >= BUFSIZE)
+
+		printf("buffer sie exceeded\n");
+	else
+		
+		buf[bufp++] = c; 
+}
+
+int getch(void);
+void ungetch(int c);
 
 struct tnode {
  char *word;
@@ -34,44 +66,13 @@ void extract(struct tnode *p);
 int freqcmp(const void *a, const void *b); 
 int getword(char *word, int lim); 
 
-int main()
+
+struct tnode *talloc(void) 
 {
-
-	struct tnode *root; 
-	char word[MAXWORD];
-	int i; 
-
-	root = NULL; 
-	while (getword(word, MAXWORD) != EOF)
-		if (isalpha(word[0]))
-			root = addtree(root, word); 
-	
-
-	tword = (struct tnode**) malloc(sizeof(struct tnode) * wordCount); 
-
-	wordCount = 0; 
-	extract(root); 
-
-	qsort(tword, wordCount, sizeof tword[0], freqcmp); 
-	
-	/* actually print these after having extract all words into tword */
-
-	for(i = 0; i < wordCount ; i++)
-
-		printf("%4d %d\n", tword[i]->count, tword[i]->word); 
-
-	return 0; 
+return (struct tnode *) malloc(sizeof(struct tnode));
 }
 
-struct tnode *talloc(void); 
-char *strdup(char *s); 
-
 struct tnode *addtree (struct tnode *p, char *w)
-
-/*
-struct tnode *talloc(void); 
- char *strdup(char *);
-*/
 
 {
 	int cond; 
@@ -82,7 +83,7 @@ struct tnode *talloc(void);
 		p->count = 1; 
 		p->left = p->right = NULL; 
 
-		wordCount++; /*count distinct words, this will be integral to completing the assignment */
+		wordCount++; /* count distinct words, this will be integral to completing the assignment */
 
 }
 	else if ((cond = strcmp(w, p->word)) == 0)
@@ -93,20 +94,8 @@ struct tnode *talloc(void);
 	 p->right = addtree(p->right, w); 
 	return p;
 }
-struct tnode *talloc (void)
-{
-return (struct tnode *) malloc(sizeof(struct tnode)); 
-}
-char *strdup(char *s) 
-{
-	char *p; 
-	if((p=(char *) malloc(strlen(s) +1)) !=NULL)
-		strcpy(p,s);
-	return p; 
-}
 
-
-void extract (struct tnode *p)
+void treeprint (struct tnode *p)
 {
 	if (p != NULL) {
 	treeprint(p->left); 
@@ -124,32 +113,27 @@ int freqcmp(const void *a, const void *b)
 	return (*q)->count - (*p) ->count;  
 }
 
-int getch(void); 
-void ungetch(int c); 
-
-
 int getword(char *word, int lim)
 {
 
 	int c; char *w = word; 
 
-	while(isspace(c = getch()))
+	while(isspace(c = getch())) 
+	;
 
-; 
+	if (c != EOF) *w++ = c; 
 
-if ( c != EOF) *w++ = c; 
-
-if(!isalpha(c)) {
-*w = '\0';
-return c; 
+	if(!isalpha(c)) {
+	*w = '\0';
+	return c; 
 }
 
-for ( ; --lim > 1; w++)
+	for ( ; --lim > 1; w++)
+	
+		if(isalnum(*w = getch())) {
 
-	if(isalnum(*w = getch())) {
-
-		ungetch(*w); 
-		break; 
+			ungetch(*w); 
+			break; 
 	}
 
 	*w = '\0';
@@ -176,3 +160,33 @@ void ungetch(int c)
 	if (bufp >= BUFSIZE)
  
 */
+
+int main()
+{
+        
+        struct tnode *root;
+        char word[MAXWORD];
+        int i;
+        
+        root = NULL; 
+        while (getword(word, MAXWORD) != EOF)
+                if (isalpha(word[0]))
+                        root = addtree(root, word);
+
+        
+        tword = (struct tnode**) malloc(sizeof(struct tnode) * wordCount);
+        
+        wordCount = 0;
+        treeprint(root);
+        
+        qsort(tword, wordCount, sizeof tword[0], freqcmp);
+        
+        /* actually print these after having extract all words into tword */
+        
+        for (i = 0; i < wordCount; i++)
+                
+                printf("%4d:%s\n", tword[i]->count, tword[i]->word);
+        
+        return 0;
+}
+
